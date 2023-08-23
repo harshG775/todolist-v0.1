e5cba138a7d25cf90fc451f800ceb1c489df7205
Harsh <hgaur491@gmail.com>
2023-08-23 14:20:08 +0530

    created my own useState in vanila js

--- src/index.js
+++ src/index.js
@@ -1,61 +1,89 @@
 "use strict";
-const $ = (e, all) =>all ? document.querySelectorAll(e) : document.querySelector(e);
+const $ = (el, all) =>all ? document.querySelectorAll(el) : document.querySelector(el);
 
-function setState(selector,callback){
-    const targetElement = $(selector)
-    let innerHtml = callback().toString()
-    function updateTargetElement(newHtml) {
-        targetElement.innerHTML=""
-        targetElement.innerHTML=newHtml
+function useState(initialState){
+    var currentState = initialState
+    function setState(callback) {
+        currentState = callback(currentState)
     }
-    updateTargetElement(innerHtml)
-}
-///////////////////////////
-
-
-
-function ListItem(prop) {
-    const { title, list,id } = prop;
-    return `
-    <li id="${id}">
-        <h4>${title}</h4>
-        <p>${list}</p>
-        <button type="button">Delete</button>
-        <button type="button">edit</button>
-    </li>
-  `;
+    return [currentState,setState]
 }
 
-let state = [
-    { title: "Task 1", list: "Do something",id:0},
-    { title: "Task 2", list: "Do another thing",id:1}
-];
 
-function ListContainer() {
-    return `
-        ${state.map(e=>(
-            ListItem(e)
-        )).join("")}
-    `;
-}
-setState("#list-container",()=>ListContainer())
-// initial render
+function app() {
+    const btn = $(".btn");
+    let [count ,setCount]= useState(1)
 
-const addListItem = (e)=>{
-    e.preventDefault()
-    const newList={
-        title:e.target[0].value,
-        list:e.target[1].value,
-        id:state.length
+    btn.addEventListener("click",()=>{
+        setCount((e)=>count+=1)
+        Render()
+    })
+    function Render() {
+        const ul = $("#list-container")
+        ul.innerHTML = count
     }
-    state=[...state,newList]
-    setState("#list-container",()=>ListContainer())
+
+    Render()
 }
-// const DeleteListItem= async () => {
-    
-//     setState("#list-container",()=>ListContainer())
+app()
+
+
+
+// ///////////////////////////
+
+// function ListItem(prop) {
+//     const { title, list,id } = prop;
+//     return `
+//     <li  id="${id}">
+//         <h4>${title}</h4>
+//         <p>${list}</p>
+//         <button type="button">Delete</button>
+//         <button type="button">edit</button>
+//     </li>
+//   `;
 // }
 
+// let state = [
+//     { title: "Task 1", list: "Do something",id:0},
+//     { title: "Task 2", list: "Do another thing",id:1}
+// ];
+
+// function ListContainer() {
+//     // $("li","all").forEach((li,index) => {
+//     //     li.addEventListener("click",(e)=>{
+//     //         // console.log(e.target.parentNode.getAttribute("id"))
+//     //         console.log("fffff")
+//     //     })
+//     // });
+//     return `
+//         ${state.map(e=>(
+//             ListItem(e)
+//         )).join("")}
+//     `;
+// }
+// setState("#list-container",()=>ListContainer())
+// // initial render ↖
+
+// function addListItem (e){
+//     e.preventDefault()
+//     const newList={
+//         title:e.target[0].value,
+//         list:e.target[1].value,
+//         id:state.length
+//     }
+//     state=[...state,newList]
+//     setState("#list-container",()=>ListContainer())
+// }
+// // function DeleteListItem(e) {
+// //     console.log(e)
+// // }
 
 
-$("form").addEventListener("submit",addListItem)
+
+
+
+
+
+
+// $("form").addEventListener("submit",addListItem)
+//     console.log($("li","all"))
